baaeba2d5bcec4f2e54ed2a74ef27ec1d23c7d68
Zeeshan Haider <z.haideraspirant@gmail.com>
2022-08-27 18:22:15 +0500

    To check running in climate dashboard - graph disabled

--- R/mod_interactive_maps.R
+++ R/mod_interactive_maps.R
@@ -256,38 +256,35 @@ mod_interactive_maps_server <- function(id, ...){
     #Plot for PCs
     #By final 5 PCs juiced up
 
-
-
-
-    output$var_explained_pcs <- shiny::renderPlot({
-
-      min_vars_for_var_exp <-  (input$features_selected)
-
-      shiny::validate(
-        shiny::need(!is.null(min_vars_for_var_exp), "Choose atleast one variable in the first tab for the chart to show up!")
-      )
-
-      sdev_id <- function(){
-        pca_scores() %>%
-          dplyr::select(-district) %>%
-          purrr::map_df(., purrr::possibly(var, NA_integer_)) %>%
-          tidyr::pivot_longer(everything(), names_to = "component", values_to = "value") %>%
-          # group_by(component) %>%
-          dplyr::mutate(
-            percent_var = value/sum(value),
-            cumsum = sum(percent_var))
-      }
-
-      sdev_id()  %>%
-        # mutate(component = forcats::fct_reorder(factor(component), percent_var)) %>%
-        ggplot2::ggplot(ggplot2::aes(component, percent_var))+
-        ggplot2::geom_col(alpha=0.5, fill = "seagreen", width = 0.4)+
-        # geom_point(aes(component,percent_var))+
-        ggplot2::labs(x="Principal Components",
-                      y="Variane Explained")+
-        ggplot2::scale_y_continuous(labels = scales::percent_format())
-
-    })
+    # output$var_explained_pcs <- shiny::renderPlot({
+    #
+    #   min_vars_for_var_exp <-  (input$features_selected)
+    #
+    #   shiny::validate(
+    #     shiny::need(!is.null(min_vars_for_var_exp), "Choose atleast one variable in the first tab for the chart to show up!")
+    #   )
+    #
+    #   sdev_id <- function(){
+    #     pca_scores() %>%
+    #       dplyr::select(-district) %>%
+    #       purrr::map_df(., purrr::possibly(var, NA_integer_)) %>%
+    #       tidyr::pivot_longer(everything(), names_to = "component", values_to = "value") %>%
+    #       # group_by(component) %>%
+    #       dplyr::mutate(
+    #         percent_var = value/sum(value),
+    #         cumsum = sum(percent_var))
+    #   }
+    #
+    #   sdev_id()  %>%
+    #     # mutate(component = forcats::fct_reorder(factor(component), percent_var)) %>%
+    #     ggplot2::ggplot(ggplot2::aes(component, percent_var))+
+    #     ggplot2::geom_col(alpha=0.5, fill = "seagreen", width = 0.4)+
+    #     # geom_point(aes(component,percent_var))+
+    #     ggplot2::labs(x="Principal Components",
+    #                   y="Variane Explained")+
+    #     ggplot2::scale_y_continuous(labels = scales::percent_format())
+    #
+    # })
 
     output$pca_download <- downloadHandler(
       filename = function(){
